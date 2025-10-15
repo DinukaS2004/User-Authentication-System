e6c37c0f2be8e21afb740d97a85457d1b1a05892
DinukS2004 <dinukad735@gmail.com>
2025-10-15 07:11:19 +0530

    Created auth routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import { connect } from 'mongoose';
 import connectDB from './config/mongodb.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials:true}))
 
+//API end points
 app.get('/',(req,res)=> res.send("API is working...."));
+app.use('/api/auth',authRouter);
 
 app.listen(port,()=> console.log(`Server running on port ${port}`))
