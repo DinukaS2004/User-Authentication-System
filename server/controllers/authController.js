83362fc9112cbb97f70fe42584481eaa61a9ba28
DinukS2004 <dinukad735@gmail.com>
2025-10-15 08:18:11 +0530

    created api for account verification

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 import transporter from '../config/nodemailer.js';
+import userModel from '../models/userModel.js';
 
 export const register  = async (req,res)=>{
     const {name,email,password} = req.body;
@@ -95,5 +96,71 @@ export const logout = (req,res)=>{
         return res.json({success:true,message:"Logged out"})
     }catch(error){
         return res.json({success:false,message:error.message})
+    }
+}
+
+
+//send verificaiton opt to the users email
+export const senVerifyOtp  = async(req,res)=>{
+    try{
+       const {userId} = req.body; 
+       const user = await userModel.findById(userId);
+        if(user.isAccountVerified){
+            return res.json({success:false,message:"Account already verified"});
+        }
+
+        const otp = String(Math.floor(100000+ Math.random()*900000));
+
+        user.verifyOtp = otp;
+        user.verifyOtpExpireAt = Date.now() + 24*60*60*1000;
+
+        await user.save();
+
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email,
+            subject: 'Account verification otp',
+            text: `Your account verification otp is: ${otp}.Verify this account using this otp.`
+        }
+
+        await transporter.sendMail(mailOptions);
+
+        res.json({success:true,message:"Verification OTP sent on email"})
+
+    }catch(error){
+        res.json({success:false,message:error.message});
+    }
+} 
+
+export const verifyEmail = async(req,res)=>{
+    const {userId} = req.body;
+
+    if(!userId || !otp){
+        return res.json({success:false,message:'Missing details'});
+    }
+    try{
+       const user = await userModel.findById(userId);
+
+       if(!user){
+        return  res.json({success:false,message:"User not found"});
+       }
+
+       if(user.verifyOtp===''|| user.verifyOtp !== otp){
+        return  res.json({success:false,message:"Invalid OTP"});
+       }
+
+       if(user.verifyOtpExpireAt <Date.now()){
+        return  res.json({success:false,message:"OTP Expired"});    
+       }
+
+       user.isAccountVerified = true;
+       user.verifyOtp = '';
+       user.verifyOtpExpireAt = 0;
+
+       await user.save();
+       return res.json({success:true,message:"Account verified successfully"});
+       
+    }catch(error){
+        res.json({success:false,message:error.message});
     }
 }
